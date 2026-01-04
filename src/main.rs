b033bc3166b818298d6fd34c28d9b7c59e9a4b5a
diogopontual <diogopontual@gmail.com>
2026-01-04 07:46:19 -0300

    echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use crate::server::Server;
+use tracing::error;
+mod server;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let server = Server::new("127.0.0.1:8080");
+    let r = server.listen().await;
+    match r {
+        Ok(_) => {}
+        Err(err) => error!("Error creating the server {}", err),
+    }
 }

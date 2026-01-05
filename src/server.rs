95783fb5826417b33397ccc6435e196fd14a5bb9
diogopontual <diogopontual@gmail.com>
2026-01-05 06:45:39 -0300

    reading events from socket

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,15 @@
 use anyhow::Result;
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpListener,
+    io::{AsyncBufReadExt, AsyncReadExt, BufReader},
+    net::{TcpListener, TcpStream},
     spawn,
 };
 use tracing::{error, info};
 
+pub struct Event {
+    topic: String,
+    payload: Vec<u8>,
+}
 pub struct Server {
     pub address: String,
 }
@@ -17,6 +21,22 @@ impl Server {
         }
     }
 
+    async fn read_event(socket: &mut TcpStream) -> Result<Event> {
+        let mut reader = BufReader::new(socket);
+        let mut buf = vec![0u8; 1024];
+        let bytes_read = reader.read_until(b'\n', &mut buf).await?;
+        let topic = std::str::from_utf8(&buf[..bytes_read])?
+            .trim_end_matches(&['\r', '\n'][..])
+            .to_string();
+        println!("Bytes Read: {} , Topic: {}", bytes_read, topic);
+        let mut length_bytes = [0u8; 4];
+        let _ = reader.read_exact(&mut length_bytes).await;
+        let length = u32::from_be_bytes(length_bytes);
+        let mut payload = vec![0u8; length as usize];
+        let _ = reader.read_exact(&mut payload).await?;
+        Ok(Event { topic, payload })
+    }
+
     pub async fn listen(&self) -> Result<()> {
         let listener = TcpListener::bind(String::from(&self.address)).await?;
         info!("The server is running on {}", &self.address);
@@ -24,18 +44,16 @@ impl Server {
             let (mut socket, addr) = listener.accept().await?;
             info!("New connection from {}", addr);
             spawn(async move {
-                let mut buf = vec![0; 1024];
                 loop {
-                    let n = match socket.read(&mut buf).await {
-                        Ok(0) => return,
-                        Ok(n) => n,
-                        Err(e) => {
-                            error!("Failed to read from socket: {}", e);
-                            return;
+                    let event_result = Server::read_event(&mut socket).await;
+                    match event_result {
+                        Ok(event) => {
+                            println!("{}", event.topic);
                         }
-                    };
-                    if socket.write_all(&buf[0..n]).await.is_err() {
-                        return;
+                        Err(err) => {
+                            error!("Error reading event from sockect: {}", err);
+                            break;
+                        }
                     }
                 }
             });
